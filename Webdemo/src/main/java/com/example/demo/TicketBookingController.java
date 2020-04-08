bb250ce22594d52cda56111745dce4e6547572ec
Mahesh <Mahesh@LAPTOP-2RPJ7E0B>
2020-04-08 22:59:17 +0530

    first commit

---
+++ Webdemo/src/main/java/com/example/demo/TicketBookingController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/")
+public class TicketBookingController {
+
+
+
+	@GetMapping(value = "/hello")
+	public String createTicket() {
+		return "hello mahesh";
+	}
+
+}
